79c4ab1add5302af1dfbe04b4e5b075426163b67
oliveira <leonidespires@gmail.com>
2023-04-14 17:25:24 -0300

    Adicionando anotação de rota para listar todos endereços.

--- src/main/java/com/pessoaDeon/controller/EnderecoController.java
+++ src/main/java/com/pessoaDeon/controller/EnderecoController.java
@@ -4,6 +4,7 @@ import com.pessoaDeon.model.Endereco;
 import com.pessoaDeon.repository.EnderecoRepository;
 import com.pessoaDeon.service.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class EnderecoController {
     @Autowired
     private EnderecoService enderecoService;
 
+    @GetMapping
     public List<Endereco> listarEnderecos(){
         return enderecoService.listarEnderecos();
     }
