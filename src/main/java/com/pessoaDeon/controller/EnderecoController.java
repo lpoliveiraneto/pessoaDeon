6156fd505f56c46b6ea0e725d4f455b17d32a372
oliveira <leonidespires@gmail.com>
2023-04-14 15:53:05 -0300

    Dados da tabela e listar todos endereco

---
+++ src/main/java/com/pessoaDeon/controller/EnderecoController.java
@@ -0,0 +1,22 @@
+package com.pessoaDeon.controller;
+
+import com.pessoaDeon.model.Endereco;
+import com.pessoaDeon.repository.EnderecoRepository;
+import com.pessoaDeon.service.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/enderecos")
+public class EnderecoController {
+
+    @Autowired
+    private EnderecoService enderecoService;
+
+    public List<Endereco> listarEnderecos(){
+        return enderecoService.listarEnderecos();
+    }
+}
