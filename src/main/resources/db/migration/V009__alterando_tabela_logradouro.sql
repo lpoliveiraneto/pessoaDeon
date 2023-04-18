d8e76e169fcee8e67f662b7c514bd8fa877dafb7
oliveira <leonidespires@gmail.com>
2023-04-18 15:51:11 -0300

    Deletando as colunas de logradouro fk_estado e fk_cidade e adicionando as colunas cidade, uf, ibge, gia, ddd, siafi.

---
+++ src/main/resources/db/migration/V009__alterando_tabela_logradouro.sql
@@ -0,0 +1,17 @@
+ALTER TABLE logradouro
+   ADD COLUMN localidade varchar(50);
+
+ALTER TABLE logradouro
+   ADD COLUMN uf varchar(3);
+
+ALTER TABLE logradouro
+   ADD COLUMN ibge varchar(10);
+
+ALTER TABLE logradouro
+   ADD COLUMN gia varchar(4);
+
+ALTER TABLE logradouro
+   ADD COLUMN ddd varchar(3);
+
+ALTER TABLE logradouro
+   ADD COLUMN siafi varchar(5);
---
+++ src/main/resources/db/migration/V010__deletando_coluna_fk_estado_cidade.sql
@@ -0,0 +1,5 @@
+ALTER TABLE logradouro
+    DROP COLUMN fk_estado CASCADE;
+
+ALTER TABLE logradouro
+    DROP COLUMN fk_cidade CASCADE;
