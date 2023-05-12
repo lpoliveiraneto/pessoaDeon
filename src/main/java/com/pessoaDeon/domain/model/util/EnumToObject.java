18f4f160dda1499700941e4b3b9a39338f6fff89
JEFF ANDRADE <lp.jeferson@hotmail.com>
2023-05-12 15:57:13 -0300

    AddClass EnumToObject

---
+++ src/main/java/com/pessoaDeon/domain/model/util/EnumToObject.java
@@ -0,0 +1,13 @@
+package com.pessoaDeon.domain.model.util;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EnumToObject {
+	
+	private String key;
+	private String value;
+
+}
