708e24dcb0d7958bcb1e0ff455715bfdaf21cbd1
oliveira <leonidespires@gmail.com>
2023-09-27 15:18:09 -0300

    Alterações feitas:
    _ Criando a classe Cargo
    - Criando a classe Funcao

---
+++ src/main/java/com/pessoaDeon/domain/model/analista/Cargo.java
@@ -0,0 +1,22 @@
+package com.pessoaDeon.domain.model.analista;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCargo;
+
+    private String descrição;
+}
