e6350be23cd2581e588f5c925f990fc33c582bb3
JEFF ANDRADE <lp.jeferson@hotmail.com>
2023-10-16 16:29:47 -0300

    add senha, login, assinatura, dataAlteracao e constraint em Analista

--- src/main/java/com/pessoaDeon/domain/model/analista/Analista.java
+++ src/main/java/com/pessoaDeon/domain/model/analista/Analista.java
@@ -1,22 +1,43 @@
 package com.pessoaDeon.domain.model.analista;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pessoaDeon.domain.model.pessoa.Pessoa;
-import jakarta.persistence.*;
+import com.pessoaDeon.domain.model.security.Perfil;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Analista {
+public class Analista implements UserDetails{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -4575354473625350361L;
 
-    @Id
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idAnalista;
 
@@ -38,9 +59,61 @@ public class Analista {
     private Integer funcionarioSigma;
 
     @JsonFormat(shape=JsonFormat.Shape.STRING, locale = "pt-BR", timezone = "Brazil/East", pattern = "yyyy-MM-dd HH:mm:ss")
-    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date data_cadastro;
+//    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime data_cadastro;
 
     private Boolean status = false;
+    
+    private String login;
+    
+    private String senha;
+    
+    @JsonFormat(shape=JsonFormat.Shape.STRING, locale = "pt-BR", timezone = "Brazil/East", pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime data_alteracao;
+    
+    private String assinatura;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "analista_perfis")
+    private List<Perfil> perfis = new ArrayList<>();
+
+    public void adicionarPerfil(Perfil perfil){
+        perfis.add(perfil);
+    }
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
 }
