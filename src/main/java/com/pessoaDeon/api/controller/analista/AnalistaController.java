b866b75a1e6a05a067532638814636df850d7113
lpoliveiraneto <leonidespires@gmail.com>
2023-10-27 14:43:39 -0300

    Alterações:
    - Criação da classe OcorrenciaController
    - retirando os metódos de AnalistaController e alocando para OcorrenciaController
    - Mudando o pacote AnaliseBoController e Ocorrencia

--- src/main/java/com/pessoaDeon/api/controller/analista/AnalistaController.java
+++ src/main/java/com/pessoaDeon/api/controller/analista/AnalistaController.java
@@ -2,87 +2,22 @@ package com.pessoaDeon.api.controller.analista;
 
 import com.pessoaDeon.domain.model.analista.Analista;
 import com.pessoaDeon.domain.model.dto.analista.AnalistaRequest;
-import com.pessoaDeon.domain.model.dto.bo.BoAnaliseRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import com.pessoaDeon.domain.model.dto.bo.BosAnalisadosResponseDto;
-import com.pessoaDeon.domain.model.dto.bo.BosPendentesResponseDto;
 import com.pessoaDeon.domain.service.analista.AnalistaService;
-import com.pessoaDeon.domain.service.analista.BoAnaliseService;
-import com.pessoaDeon.domain.service.bo.BoService;
-
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/ocorrencia")
+@RequestMapping("/api/v1/analista")
 public class AnalistaController {
-
     @Autowired
-    private BoService boService;
+    AnalistaService analistaService;
 
-    @Autowired
-    private BoAnaliseService boAnaliseService;
-    
-    @Autowired
-    private AnalistaService analistaService;
-
-    @GetMapping
-    public Page<BosPendentesResponseDto> listarOcorrenciasParaAnalise(
-    		@PageableDefault(size = 10, page = 0, sort = "idBo", 
-    		direction = Sort.Direction.ASC)Pageable pageable){
-        return boService.getBosPendentes(pageable);
-    }
-
-    @GetMapping("/todos")
-    public Page<BosAnalisadosResponseDto> listarBosAnaliseTodos(
-            @PageableDefault(size = 10, page = 0, sort = "idBo",
-                    direction = Sort.Direction.ASC)Pageable pageable){
-
-        return boAnaliseService.getBoAnalise(pageable);
-    }
-
-    @GetMapping("/analisadas")
-    public Page<BosAnalisadosResponseDto> listarBosAnalisados(
-            @PageableDefault(size = 10, page = 0, sort = "idBo",
-                    direction = Sort.Direction.ASC)Pageable pageable){
-
-        return boAnaliseService.getBoAnalisados(pageable);
-    }
-
-    @GetMapping("/emAnalise")
-    public Page<BosAnalisadosResponseDto> listarBosEmAnalise(
-            @PageableDefault(size = 10, page = 0, sort = "idBo",
-                    direction = Sort.Direction.ASC)Pageable pageable){
-        return boAnaliseService.getBoEmAnalise(pageable);
-    }
-
-    @PostMapping("/emAnalise")
-    public ResponseEntity salvarBoAnalise(@RequestBody BoAnaliseRequest boAnaliseRequest){
-        System.out.println("irei salvar na tabela BoAnalise");
-        boAnaliseService.salvarBoEmAnalise(boAnaliseRequest);
-        return ResponseEntity.ok().build();
-    }
-
-    @PutMapping("/emAnalise")
-    public ResponseEntity salvarRespostaBoAnalise(@RequestBody BoAnaliseRequest boAnaliseRequest){
-        boAnaliseService.salvarRespostaBoEmAnalise(boAnaliseRequest);
-       // System.out.println("testando rota.");
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/funcionario/analisadas")
-    public ResponseEntity<?>  listarOcorrenciasAnalisadasFuncionario(){
-        return ResponseEntity.ok().build();
-    }
-    
     @PostMapping("/salvarAnalista")
     public Analista salvarAnalista(@RequestBody AnalistaRequest analistaRequest, HttpServletRequest http) {
-    	return analistaService.salvarAnalista(analistaRequest, http);
+        return analistaService.salvarAnalista(analistaRequest, http);
     }
 }
---
+++ src/main/java/com/pessoaDeon/api/controller/ocorrencia/OcorrenciaController.java
@@ -0,0 +1,82 @@
+package com.pessoaDeon.api.controller.ocorrencia;
+
+import com.pessoaDeon.domain.model.dto.bo.BoAnaliseRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.pessoaDeon.domain.model.dto.bo.BosAnalisadosResponseDto;
+import com.pessoaDeon.domain.model.dto.bo.BosPendentesResponseDto;
+import com.pessoaDeon.domain.service.analista.AnalistaService;
+import com.pessoaDeon.domain.service.analista.BoAnaliseService;
+import com.pessoaDeon.domain.service.bo.BoService;
+
+
+@RestController
+@RequestMapping("/api/v1/ocorrencia")
+public class OcorrenciaController {
+
+    @Autowired
+    private BoService boService;
+
+    @Autowired
+    private BoAnaliseService boAnaliseService;
+    
+    @Autowired
+    private AnalistaService analistaService;
+
+    @GetMapping
+    public Page<BosPendentesResponseDto> listarOcorrenciasParaAnalise(
+    		@PageableDefault(size = 10, page = 0, sort = "idBo", 
+    		direction = Sort.Direction.ASC)Pageable pageable){
+        return boService.getBosPendentes(pageable);
+    }
+
+    @GetMapping("/todos")
+    public Page<BosAnalisadosResponseDto> listarBosAnaliseTodos(
+            @PageableDefault(size = 10, page = 0, sort = "idBo",
+                    direction = Sort.Direction.ASC)Pageable pageable){
+
+        return boAnaliseService.getBoAnalise(pageable);
+    }
+
+    @GetMapping("/analisadas")
+    public Page<BosAnalisadosResponseDto> listarBosAnalisados(
+            @PageableDefault(size = 10, page = 0, sort = "idBo",
+                    direction = Sort.Direction.ASC)Pageable pageable){
+
+        return boAnaliseService.getBoAnalisados(pageable);
+    }
+
+    @GetMapping("/emAnalise")
+    public Page<BosAnalisadosResponseDto> listarBosEmAnalise(
+            @PageableDefault(size = 10, page = 0, sort = "idBo",
+                    direction = Sort.Direction.ASC)Pageable pageable){
+        return boAnaliseService.getBoEmAnalise(pageable);
+    }
+
+    @PostMapping("/emAnalise")
+    public ResponseEntity salvarBoAnalise(@RequestBody BoAnaliseRequest boAnaliseRequest){
+        System.out.println("irei salvar na tabela BoAnalise");
+        boAnaliseService.salvarBoEmAnalise(boAnaliseRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/emAnalise")
+    public ResponseEntity salvarRespostaBoAnalise(@RequestBody BoAnaliseRequest boAnaliseRequest){
+        boAnaliseService.salvarRespostaBoEmAnalise(boAnaliseRequest);
+       // System.out.println("testando rota.");
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/funcionario/analisadas")
+    public ResponseEntity<?>  listarOcorrenciasAnalisadasFuncionario(){
+        return ResponseEntity.ok().build();
+    }
+    
+
+}
