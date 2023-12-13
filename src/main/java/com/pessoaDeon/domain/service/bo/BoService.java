6c83e099d3c2ad05ca63e1ac74dbddef9349db37
oliveira <leonidespires@gmail.com>
2023-12-13 17:12:34 -0300

    Alterações feitas:
    - retirando import não mais usado

--- src/main/java/com/pessoaDeon/domain/service/bo/BoService.java
+++ src/main/java/com/pessoaDeon/domain/service/bo/BoService.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
-import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.Predicate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
